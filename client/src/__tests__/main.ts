f84bb713ab39e7a8d699e0c4197e17870a01a0fc
BlueBoi904 <cliff.mirschel@gmail.com>
2021-06-09 20:45:31 -0700

    Added tailwind

--- client/src/__tests__/main.ts
+++ client/src/__tests__/main.ts
@@ -1,7 +1,24 @@
-function sum(a, b) {
+type SumArgs = {
+  a: number;
+  b: number;
+};
+
+function sum(a, b): SumArgs {
   return a + b;
 }
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+type AddOneArgs = {
+  num: number;
+};
+
+function addOne(num): AddOneArgs {
+  return num + 1;
+}
+
+test('add 1', () => {
+  expect(addOne(1)).toBe(2);
 });
