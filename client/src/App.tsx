8243d4ea9b9f70e9d82b99078f207012334e711f
BlueBoi904 <cliff.mirschel@gmail.com>
2021-06-19 18:06:00 -0700

    Removed tailwind forms

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
+import { LoginPage } from "./pages/LoginPage";
+
 function App() {
   return (
-    <div className=" min-h-screen container mx-auto px-4 font-mono h-auto bg-yellow-300">
-      <p>hello world</p>
+    <div className=" min-h-screen container mx-auto px-4 h-auto w-auto bg-yellow-300">
+      <LoginPage />
     </div>
   );
 }
---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,14 @@
+function LoginPage() {
+  return (
+    <div className="">
+      <form>
+        <label className="block">
+          <span className="text-gray-700">Username</span>
+          <input className="form-input mt-1 block" placeholder="Jane Doe" />
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export { LoginPage };
